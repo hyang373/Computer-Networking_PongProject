3b3320dd897840afc6f34f79eebeccaea21a4ccc
asmitakarki <117233538+asmitakarki@users.noreply.github.com>
2023-10-15 12:15:44 -0400

    Update pongServer.py
    added code to assign players using a queue

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -8,19 +8,32 @@
 
 import socket
 import threading
+from queue import Queue
 
 # Use this file to write your server logic
-
 
 # server is a socket object
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # this is creating the server
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # working on local host need
 
+# create a queue and set max size to 2
+player_in_queue = Queue(maxsize=2)
 
-# Create a function to handle each client
-def handle_client(clientSocket):
-    # Implement the game logic and communication here
-    pass
+# Function to handle each client
+def handle_client(clientSocket, player_num):
+    while True:
+        # Implement game logic and communication here
+        data = clientSocket.recv(1024)  # Receive data from the client
+        if not data:
+            break  # Connection closed by the client
+        # Process the data and update game state
+    clientSocket.close()
+
+    
+if (player_in_queue.full()):
+    player1 = player_in_queue.get()
+    player2 = player_in_queue.get()
+    print(f"Player 1: {player1} Player 2: {player2}")
 
 #  IP = 128.163.35.46 
 server.bind(('10.113.33.101', 12345)) # binds server to a specific IP address and port
@@ -33,6 +46,7 @@ while True:
     # Start a new thread to handle the client
     client_thread = threading.Thread(target=handle_client, args=(clientSocket,))
     client_thread.start()
+    
 # accept incoming connections
 
 
