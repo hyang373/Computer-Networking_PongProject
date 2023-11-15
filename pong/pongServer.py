efeaf6539d424375f538a0d187dc8aca73047e2d
Helen Yang <84105597+hyang373@users.noreply.github.com>
2023-11-15 16:46:03 -0500

    Update pongServer.py
    ball missing, paddles are in sync, scores missing

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -1,80 +1,74 @@
 import socket
 import threading
- 
+import json
+
 # Globol Variables
 clientSocket0: socket
 clientSocket1: socket
 SIZE: int
  
-def client0Thread():
-    print("Client1 Thread Function Entered")
+def client_thread(client_socket, target_socket):
     try:
         while True:
-            # get data from clients
-            data = clientSocket0.recv(SIZE)
-            if data == b"finished":
-                return
-            clientSocket1.send(data)
-    except:
-        return
- 
-def client1Thread():
-    print("Client2 Thread Function Entered")
-    try:
-        while True:
-            data = clientSocket1.recv(SIZE)
-            if data == b"finished":
-                return
-            clientSocket0.send(data)
-    except:
-        return
+            try:
+                data = client_socket.recv(SIZE)
+                print(f"Received data: {data}")
+                client_data = json.loads(data.decode("utf-8"))
+                target_socket.send(json.dumps(client_data).encode("utf-8"))
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+
+    except Exception as e:
+        print(f"Client thread error: {e}")
+    finally:
+        print("Closing client socket.")
+        client_socket.close()
+
  
 def startServer():
-
-    # local ip address & port
-    localIP = "10.113.33.191"
-    localPort = 12345
-    
-    # connect server to ip and port number
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((localIP, localPort))
-    
-    while True: 
-
-        server.listen(2)    # number of clients waiting in queue for for "accept"
-                            # If queue is full then client can't connect.
-        
+    try:
+        # local ip address & port
+        localIP = "10.113.33.191"
+        localPort = 12345
+   
+        # connect server to ip and port number
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((localIP, localPort))
+        server.listen(2)    # number of clients waiting in queue for for "accept"                
+        # If queue is full then client can't connect.
+   
         print("Use this IP in clients: " + localIP)
-        
-        SIZE = 1024
+   
+        #SIZE = 1024
         
         # accept the two client's socket and address in queue
         clientSocket0, clientAddress0 = server.accept()
         clientSocket1, clientAddress1 = server.accept()
-        
-        print(f"Client1: {clientSocket0} \nClient2: {clientSocket1}" )
-        
+   
         # Send player information to clients
-        data = "screen 640 480 player1 left player2 right"
-        data1 = "screen 640 480 player1 right player2 left"
-        print(data)
+        data = "screen 640 480 left"
+        data1 = "screen 640 480 right"
         clientSocket0.send(data.encode("utf-8")[:1024])
         clientSocket1.send(data1.encode("utf-8")[:1024])
+   
 
-        thread0 = threading.Thread(target=client0Thread)
-        thread1 = threading.Thread(target=client1Thread)
-        
+        thread0 = threading.Thread(target=client_thread, args=(clientSocket0, clientSocket1))
+        thread1 = threading.Thread(target=client_thread, args=(clientSocket1, clientSocket0))
+       
+   
         thread0.start()
         thread1.start()
-        print("threads started")
-
+   
         thread0.join()
         thread1.join()
-        print("threads joined")
-        
+   
         clientSocket0.close()
         clientSocket1.close()
-
+    except Exception as e:
+        print(f"Server error: {e}")
+    finally:
+        server.close()
+ 
 if __name__ == "__main__":
-
+    SIZE = 1024
     startServer()
